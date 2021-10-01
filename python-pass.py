59d719b94abb1b3cf1d535c2707a4688f37ce743
Khadija Al-Khashab <khadija.alkhashab@Khadija.local>
2021-10-01 08:04:35 +0300

    solution

--- python-pass.py
+++ python-pass.py
@@ -1,23 +1,21 @@
 class Solution:
 
-    def longest_palindromic(s: str) -> str:
-        # Create a string to store our resultant palindrome
-        palindrome = ''
+  def longest_palindromic(s: str) -> str:
+    # Create a string to store the result
+    palindrome = ''
 
-        # loop through the input string
-        for i in range(len(s)):
+    for i in range(len(s)):
 
-            # loop backwards through the input string
-            for j in range(len(s), i, -1):
+      for j in range(len(s), i, -1):
 
-                # Break if out of range
-                if len(palindrome) >= j - i:
-                    break
+        # break
+        if len(palindrome) >= j - i:
+            break
 
-                # Update variable if matches
-                elif s[i:j] == s[i:j][::-1]:
-                    palindrome = s[i:j]
-                    break
+        # Update if match is found
+        elif s[i:j] == s[i:j][::-1]:
+            palindrome = s[i:j]
+            break
 
-        return palindrome
-    print(longest_palindromic("cbaba"))
+      return palindrome
+  print(longest_palindromic("cbaba"))
